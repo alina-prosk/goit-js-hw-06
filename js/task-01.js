645c87063e042338f45802d69fb669c060c6bf3a
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-03 14:04:43 +0300

    1

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+const navEl = document.querySelector('#categories');
+console.log(navEl);
+// console.log(navEl.children);
+
+// const navItemEl = document.querySelectorAll('.item');
+// console.log(navItemEl);
+
+// const text = document.querySelector('#categories');
+// console.log(text.textContent);
+const getNumbersOfCategories = () => {
+    console.log(`Category ${itemsEl.length}`)
+}
+
+{
+    constnumberOfEl = [];
+    array.forEach(navEl.length => {
+      nu  
+    });
+}
