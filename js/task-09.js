90351c630bd8b4721e45f54aa1eb15fefaa3a5ff
Alina Proskurnitska <proskur1987@gmail.com>
2022-08-05 18:33:28 +0300

    task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+}
+
+refs.button.addEventListener('click', () => {
+  const getHexColor = getRandomHexColor();
+  document.body.style.backgroundColor = getHexColor;
+  
+  refs.span.textContent = getHexColor;
+});
+
+
